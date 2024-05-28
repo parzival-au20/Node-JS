f88342362786a659f94043b30f1c43be96bcc596
parzival-au20 <74263381+parzival-au20@users.noreply.github.com>
2024-05-28 13:51:12 +0300

    Delete btk directory

--- btk/app.js
+++
@@ -1,8 +0,0 @@
-const Logger = require('./logger');
-const logger = new Logger();
-
-logger.on('connection',function(args){
-    console.log('baglantı kuruldu.',args);
-});
-
-logger.log('User login oldu.');
